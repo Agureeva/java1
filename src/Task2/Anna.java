f27943bc43e44e1ccccbcf09b357f4e75da62702
Anna Agureeva <anagurr@gmail.com>
2020-05-27 23:49:23 +0300

    Start

---
+++ src/Task2/Anna.java
@@ -0,0 +1,13 @@
+package Task2;
+public class Anna {
+    static double fractional(double num1) {
+        num1 = % 1;
+//        int i1 = (int) num1;
+//        double doub2 = num1 - i1;
+//        return doub2;
+    }
+    public static void main(String[] args) {
+        double frac1 = fractional(146.357);
+        System.out.println(frac1);
+    }
+}
